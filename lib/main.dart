3e82461fe9525227c95df5ef42c2d92b392fc858
girithc <girithmchoudhary@gmail.com>
2023-09-18 16:26:00 +0530

    intro animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:pronto/home/home_screen.dart';
 import 'package:pronto/login/phone_screen.dart';
 import 'package:provider/provider.dart';
-
 import 'cart/cart.dart';
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool isLoggedIn = false;
+  bool? isLoggedIn;
 
   @override
   void initState() {
@@ -37,29 +37,99 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    // Using MultiProvider is convenient when providing multiple objects.
     return MultiProvider(
       providers: [
-        // In this sample app, CatalogModel never changes, so a simple Provider
-        // is sufficient.
         Provider(create: (context) => CartModel()),
-        // CartModel is implemented as a ChangeNotifier, which calls for the use
-        // of ChangeNotifierProvider. Moreover, CartModel depends
-        // on CatalogModel, so a ProxyProvider is needed.
-
         ChangeNotifierProvider<CartModel>(
           create: (context) => CartModel(),
         ),
       ],
       child: MaterialApp(
-          title: 'Provider Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: (isLoggedIn)
-              ? const MyHomePage(title: 'Pronto')
-              : const MyPhone()),
+        title: 'Provider Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: (isLoggedIn == null)
+            ? const CircularProgressIndicator() // Loading indicator while checking login status
+            : OpeningPageAnimation(isLoggedIn: isLoggedIn!),
+      ),
+    );
+  }
+}
+
+class OpeningPageAnimation extends StatefulWidget {
+  final bool isLoggedIn;
+
+  const OpeningPageAnimation({required this.isLoggedIn, Key? key})
+      : super(key: key);
+
+  @override
+  _OpeningPageAnimationState createState() => _OpeningPageAnimationState();
+}
+
+class _OpeningPageAnimationState extends State<OpeningPageAnimation> {
+  late double _begin;
+  late double _end;
+
+  @override
+  void initState() {
+    super.initState();
+    _begin = -0.5;
+    _end = 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TweenAnimationBuilder<double>(
+              tween: Tween<double>(begin: _begin, end: _end),
+              duration: const Duration(seconds: 4, microseconds: 20),
+              builder: (BuildContext context, double position, Widget? child) {
+                return Transform.translate(
+                  offset: Offset(
+                    position * MediaQuery.of(context).size.width,
+                    0,
+                  ),
+                  child: Transform.scale(
+                    scale: 0.75,
+                    child: child!,
+                  ),
+                );
+              },
+              onEnd: () {
+                if (widget.isLoggedIn) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const MyHomePage(title: 'Pronto')));
+                } else {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const MyPhone()));
+                }
+              },
+              child: Image.asset('assets/images/scooter.avif'),
+            ),
+            const SizedBox(
+                height:
+                    16), // Provide a bit of spacing between the image and the text
+            Animate(
+              effects: const [FadeEffect(), ScaleEffect()],
+              child: const Text(
+                "Grocery Delivery In Minutes.",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
