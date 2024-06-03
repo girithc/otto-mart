52840cbc597540c5f1f6e16adbbca87818bccc12
girithc <girithmchoudhary@gmail.com>
2024-06-03 19:09:57 +0530

    changes

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   file_selector_windows
   firebase_core
-  firebase_storage
   flutter_secure_storage_windows
   smart_auth
   url_launcher_windows
